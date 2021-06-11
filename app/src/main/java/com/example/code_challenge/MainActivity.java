54f1bb7d9c5416b1997e6220e2da15b378124064
Aaryan Pagar <2597573P@student.gla.ac.uk>
2021-06-12 02:54:22 +0530

    First Commit - Main Activity and Basic Layouts

--- app/src/main/java/com/example/code_challenge/MainActivity.java
+++ app/src/main/java/com/example/code_challenge/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.code_challenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    RelativeLayout niche_tasks;
+    RelativeLayout reminders;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        niche_tasks = findViewById(R.id.niche_tasks);
+        reminders = findViewById(R.id.reminders);
+
+        niche_tasks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startIntent(NicheTasks.class);
+            }
+        });
+
+        reminders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startIntent(Reminders.class);
+            }
+        });
+    }
+
+    private void startIntent(Class pass){
+        Intent intent = new Intent(getApplicationContext(), pass);
+        startActivity(intent);
     }
 }
