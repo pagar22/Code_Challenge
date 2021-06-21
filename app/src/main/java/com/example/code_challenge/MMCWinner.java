24572b22ddb06aa31b0b6b54851020086e67bc27
Aaryan Pagar <2597573P@student.gla.ac.uk>
2021-06-22 02:27:29 +0530

    Seventh Commit - Added NotificationBuilder and Entire MyMommy League code !Check for bugs!

---
+++ app/src/main/java/com/example/code_challenge/MMCWinner.java
@@ -0,0 +1,134 @@
+package com.example.code_challenge;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+public class MMCWinner extends AppCompatActivity {
+
+    private TextView congratulationsText;
+    private TextView MMCWinnerName;
+    private TextView descriptionText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mmc_winner);
+
+        congratulationsText = findViewById(R.id.congratulationsText);
+        MMCWinnerName = findViewById(R.id.MMCWinnerName);
+        descriptionText = findViewById(R.id.descriptionText);
+
+    }
+
+    public void showGif(View view) {
+        Button button = findViewById(R.id.buttonWinner);
+        button.setVisibility(View.INVISIBLE);
+        button.setClickable(false);
+
+        ImageView fireworks1 = findViewById(R.id.fireworks1);
+        Glide.with(this).load(R.drawable.fireworks).into(fireworks1);
+
+        ImageView fireworks2 = findViewById(R.id.fireworks2);
+        Glide.with(this).load(R.drawable.fireworks).into(fireworks2);
+
+        onSeasonEnd(getApplicationContext());
+
+    }
+
+    public void onSeasonEnd(Context context){
+
+        SharedPreferences sharedPreferencesAppend = context.getSharedPreferences("Code_Challenge", MODE_APPEND);
+        int familyMembers = sharedPreferencesAppend.getInt("members", 0);
+
+        //get the winner
+        String maxMember = "";
+        int maxPoints = -1;
+        boolean tie = false;
+        for (int i = 1; i <= familyMembers; i++) {
+            String memberName = sharedPreferencesAppend.getString("member" + i + "Name", "");
+            if (!memberName.equals("")) {
+                int memberPoints = sharedPreferencesAppend.getInt(memberName + "PointsOnSeasonEnd", -1);
+                //if PointsOnSeasonEnd was never set (in Splash or PointsTally), take the value as current points instead
+                if(memberPoints==-1)
+                    memberPoints = sharedPreferencesAppend.getInt(memberName + "Points", 0);
+
+                if (memberPoints > maxPoints) {
+                    maxMember = memberName;
+                    maxPoints = memberPoints;
+                    tie = false;
+                } else if (memberPoints == maxPoints) {
+                    maxMember += " & " + memberName;
+                    tie = true;
+                }
+
+            }
+        }
+
+        //create congratulatory strings & increment MMC wins
+        SharedPreferences.Editor editor = context.getSharedPreferences("Code_Challenge", MODE_PRIVATE).edit();
+
+        int mmcSeason = sharedPreferencesAppend.getInt("seasonMMC", 1);
+        String incentiveDetailsFull = sharedPreferencesAppend.getString(mmcSeason + "Incentive", "");
+        String incentiveDetails = "";
+        //both are mandatory fields and will be found
+        int startIndex = incentiveDetailsFull.indexOf("General Incentive");
+        int endIndex = incentiveDetailsFull.lastIndexOf("Duration");
+        System.out.println("Start - "+startIndex + " End - "+endIndex);
+        if (startIndex != -1 && endIndex != -1)
+            incentiveDetails = incentiveDetailsFull.substring(startIndex, endIndex);
+        String description;
+        if (tie) {
+            String[] allWinners = maxMember.split(" & ");
+            for (String s : allWinners) {
+                int currentMMCWins = sharedPreferencesAppend.getInt(s + "MMC", 0);
+                editor.putInt(s + "MMC", currentMMCWins + 1);
+            }
+            description = '\n' + "With " + maxPoints + " points, " + maxMember + " have won MyMommy Season # " + mmcSeason
+                    + ". Winners are now eligible to claim the following rewards: " + '\n'
+                    + incentiveDetails + '\n'
+                    + "Keep putting similar effort into vital housework tasks to win more MyMommy Cups."
+                    + " All the best for future seasons!" + '\n'
+                    + "- The MyMommy Team";
+        } else {
+            int currentMMCWins = sharedPreferencesAppend.getInt(maxMember + "MMC", 0);
+            editor.putInt(maxMember + "MMC", currentMMCWins + 1);
+            description = '\n' + "With " + maxPoints + " points, " + maxMember + " has won MyMommy Season # " + mmcSeason
+                    + ". You are now eligible to claim the following rewards: " + '\n'
+                    + incentiveDetails + '\n'
+                    + "You have won" + (currentMMCWins + 1) + " MyMommy Cups. Keep putting similar effort into vital housework tasks to win more."
+                    + " All the best for future seasons!" + '\n'
+                    + "- The MyMommy Team";
+        }
+
+        MMCWinnerName.setText(maxMember);
+        descriptionText.setText(description);
+
+        //change system status to new MyMommy
+        editor.putInt("seasonMMC", mmcSeason + 1);
+        editor.putBoolean("ongoingMMC", false);
+        editor.putString(mmcSeason + "Incentive", "");
+        editor.putLong("actionTimeMMC", Long.MAX_VALUE);
+
+        //reset all points to zero
+        for(int i=1; i<=familyMembers; i++){
+            String memberName = sharedPreferencesAppend.getString("member"+i+"Name", "");
+            editor.putInt(memberName + "Points", 0);
+            editor.remove(memberName + "PointsOnSeasonEnd");
+        }
+
+        editor.commit();
+
+        congratulationsText.setVisibility(View.VISIBLE);
+        MMCWinnerName.setVisibility(View.VISIBLE);
+        descriptionText.setVisibility(View.VISIBLE);
+
+    }
+}
---
+++ app/src/main/java/com/example/code_challenge/NotificationBroadcastBuilder.java
@@ -0,0 +1,37 @@
+package com.example.code_challenge;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class NotificationBroadcastBuilder extends BroadcastReceiver {
+
+    final int NOTIFICATION_ID = 100;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        int seasonNumber = intent.getIntExtra("season_number", 1);
+        Intent tempIntent = new Intent(context, MMCWinner.class);
+        PendingIntent onClickIntent = PendingIntent.getActivity(
+                context, NOTIFICATION_ID,
+                tempIntent, PendingIntent.FLAG_UPDATE_CURRENT
+        );
+
+        NotificationCompat.Builder builder= new NotificationCompat.Builder(context, "code_challenge")
+                .setSmallIcon(R.drawable.my_mommy_icon)
+                .setContentTitle("MyMommy Season # " + seasonNumber+ " Results!")
+                .setContentText("The current MyMommy season is now complete! Click here to see who topped the charts.")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(onClickIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+
+    }
+}
